bf2a97fe779c84fd3e627ff9ad49e039fd488c61
Mohamed Oussama Benkaddour <mohamedoussama2k05@gmail.com>
2026-03-06 21:32:04 +0000

    Create exercice7.js

---
+++ exercice7.js
@@ -0,0 +1,17 @@
+let nom = "Oussama";
+let age = 22;
+let email = "oussama@email.com";
+let estEtudiant = true;
+let score = 85;
+
+console.log("Nom :", nom);
+console.log("Age :", age);
+console.log("Email :", email);
+console.log("Etudiant :", estEtudiant);
+console.log("Score :", score);
+
+if (score >= 50) {
+    console.log("Résultat : Admis");
+} else {
+    console.log("Résultat : Echoué");
+}
