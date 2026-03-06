ba51fff4643650cc5cc54cf2734f11b372f4e1d6
Mohamed Oussama Benkaddour <mohamedoussama2k05@gmail.com>
2026-03-06 21:30:29 +0000

    Create exercice4.js

---
+++ exercice4.js
@@ -0,0 +1,15 @@
+let a = 5;
+let b = "5";
+let c = true;
+let d = 1;
+let e = null;
+let f = undefined;
+
+console.log("5 == '5' :", a == b);
+console.log("5 === '5' :", a === b);
+
+console.log("true == 1 :", c == d);
+console.log("true === 1 :", c === d);
+
+console.log("null == undefined :", e == f);
+console.log("null === undefined :", e === f);
