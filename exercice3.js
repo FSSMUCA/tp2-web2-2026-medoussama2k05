0dfc63d679ad92db2eeba5165fb9f68ab9565263
Mohamed Oussama Benkaddour <mohamedoussama2k05@gmail.com>
2026-03-06 21:29:54 +0000

    Create exercice3.js

---
+++ exercice3.js
@@ -0,0 +1,17 @@
+let v1 = 42;
+let v2 = "JS";
+let v3 = true;
+let v4 = null;
+let v5 = undefined;
+let v6 = { a: 1 };
+let v7 = [1,2,3];
+let v8 = NaN;
+
+console.log("v1:", typeof v1);
+console.log("v2:", typeof v2);
+console.log("v3:", typeof v3);
+console.log("v4:", typeof v4);
+console.log("v5:", typeof v5);
+console.log("v6:", typeof v6);
+console.log("v7:", typeof v7);
+console.log("v8:", typeof v8);
