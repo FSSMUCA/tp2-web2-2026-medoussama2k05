623267c2d84969507707129e8dc2f8388203dde8
Mohamed Oussama Benkaddour <mohamedoussama2k05@gmail.com>
2026-03-06 21:28:35 +0000

    Create exercice1.js

---
+++ exercice1.js
@@ -0,0 +1,15 @@
+let a = 10;
+let b = "Bonjour";
+let c = true;
+let d = null;
+let e;
+let f = { nom: "Ali" };
+let g = [1, 2, 3];
+
+console.log("a ->", typeof a);
+console.log("b ->", typeof b);
+console.log("c ->", typeof c);
+console.log("d ->", typeof d);
+console.log("e ->", typeof e);
+console.log("f ->", typeof f);
+console.log("g ->", typeof g);
