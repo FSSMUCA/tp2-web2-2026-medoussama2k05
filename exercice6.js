8385ec2b3ba9dc54ad73f2012960c3de2820e3ed
Mohamed Oussama Benkaddour <mohamedoussama2k05@gmail.com>
2026-03-06 21:31:31 +0000

    Create exercice6.js

---
+++ exercice6.js
@@ -0,0 +1,11 @@
+let a = null;
+let b = undefined;
+let c = 0;
+let d = "";
+let e = "Bonjour";
+
+console.log("a :", a ?? "valeur par défaut");
+console.log("b :", b ?? "valeur par défaut");
+console.log("c :", c ?? "valeur par défaut");
+console.log("d :", d ?? "valeur par défaut");
+console.log("e :", e ?? "valeur par défaut");
